f0e01b4610942f44901cb32584e23ee762086097
COTTUREC <corentin.cttr@eduge.ch>
2019-01-21 15:19:55 +0100

    lvl 1 Ajout du TDD

---
+++ CarrefourEssert/TestUnititaire/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnititaire")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnititaire")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f716ccca-7c97-4087-a27b-c2710ae42347")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CarrefourEssert/TestUnititaire/TestUnitaireClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnititaire
+{
+    [TestClass]
+    public class TestUnitaireClass
+    {
+        /***************************************
+        **************** Routes ****************
+        ***************************************/
+        [TestMethod]
+        public void TestClassRoutes()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        ***************** Feu ******************
+        ***************************************/
+        [TestMethod]
+        public void TestClassFeu()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        *************** Véhicule ***************
+        ***************************************/
+        [TestMethod]
+        public void TestClassVehicule()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        ***************** Tram *****************
+        ***************************************/
+        [TestMethod]
+        public void TestClassTram()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        ***************** Moto *****************
+        ***************************************/
+        [TestMethod]
+        public void TestClassMoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        ***************** Velo *****************
+        ***************************************/
+        [TestMethod]
+        public void TestClassVelo()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        **************** Voiture ***************
+        ***************************************/
+        [TestMethod]
+        public void TestClassVoiture()
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************
+        ****************** Bus *****************
+        ***************************************/
+        [TestMethod]
+        public void TestClassBus()
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
