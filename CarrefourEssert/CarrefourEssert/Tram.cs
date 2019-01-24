166350df30369f26b9d4c6d826d214e38693209b
DanySer <dany.serigado7@gmail.com>
2019-01-24 15:34:18 +0100

    Ajout methodes

---
+++ CarrefourEssert/CarrefourEssert/Tram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConFour
+{
+    class Tram : Vehicules
+    {
+        public Tram(int pNbRoues, double pTaille, bool pAvancer, bool pReculer, string pImmatriculation, string pCouleur, string pMarque, double pPosX, double pPosY) : base() : base(pNbRoues, pTaille, pAvancer, pReculer, pImmatriculation, pCouleur, pMarque, pPosX, pPosY)
+        {
+
+        }
+    }
+
